17a551ee53870af87a0291592a435a4abc3d1d9b
mocko <daniel.schmidtke@web.de>
2021-04-06 15:30:13 +0200

    fixed .env variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useAnimals from "./useAnimals";
 
 function App() {
+  useAnimals();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>
+          <div className="wrapper"></div>
+          <span>React and Contentful</span>
+        </header>
+        <main>
+          <div className="wrapper"></div>
+        </main>
+      </div>
     </div>
   );
 }
