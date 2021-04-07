16f7e64f9b150ddd7689e84c41a116773c3448bf
Roberto Ubieto Juan <robertouj@gmail.com>
2021-04-07 17:45:19 +0200

    first steps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,71 @@
 import "./App.css";
 import useAnimals from "./useAnimals";
+import React, { useState } from "react";
+//import { Container, Row, Col } from "reactstrap";
+import {
+  Button,
+  ButtonGroup,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption,
+} from "reactstrap";
+import AnimalsCarousel from "./components/AnimalsCarousel";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  useAnimals();
+  const animals = useAnimals();
+  const [modal, setModal] = useState(false);
+  //console.log(animals);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <div className="App">
       <div className="container">
         <header>
           <div className="wrapper"></div>
-          <span>React and Contentful</span>
+          <span>Astonishing Animals</span>
         </header>
-        <main>
-          <div className="wrapper"></div>
+        <main className="Animal">
+          <div className="wrapper">
+            <div className="AnimalList">
+              {animals.map((animal) => {
+                return (
+                  <div className="Animal">
+                    <h3>{animal.name}</h3>
+                    <h2>{animal.latinName}</h2>
+                    <img src={animal.img} alt={animal.title} />
+                    <ButtonGroup>
+                      <Button onClick={toggle}>Images</Button>
+                      <Button>Video</Button>
+                    </ButtonGroup>
+                  </div>
+                );
+              })}
+              <div className="Modal">
+                <Modal isOpen={modal} toggle={toggle}>
+                  <ModalHeader
+                    className="modal__header"
+                    toggle={toggle}
+                  ></ModalHeader>
+                  <ModalBody className="modal__body row">
+                    <AnimalsCarousel animals={animals} />
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="secondary" onClick={toggle}>
+                      Cancel
+                    </Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
+            </div>
+          </div>
         </main>
       </div>
     </div>
--- src/useAnimals.js
+++ src/useAnimals.js
@@ -1,12 +1,85 @@
 import { useEffect, useState } from "react";
 import { client } from "./client";
 
+const getAnimal = (animal) => ({
+  name: animal.fields.name,
+  latinName: animal.fields.latinName,
+  thumbnails: animal.fields.thumbnails?.map((thumb) => ({
+    url: thumb.fields.file.url,
+    title: thumb.fields.title,
+  })),
+  crop: animal.fields.image.fields.file.url + "?w=300&h=200&fit=crop",
+  img: animal.fields.image.fields.file.url + "?w=400&h=300&fit=fill",
+});
+
 function useAnimals() {
+  const [animals, setAnimals] = useState([]);
+
   useEffect(() => {
     client.getEntries().then((response) => {
-      console.log(response);
+      const newAnimals = response.items.map((item) => getAnimal(item));
+      setAnimals(newAnimals);
     });
-  });
+  }, []);
+
+  /* // 
+
+    //`string text ${expression} string text`
+    // animals[0].fields.image.fields.file.url
+    //images.ctfassets.net/utnfcxl3mjrn/50p1o2nZkNiQZYZGs1308H/48e8b02fa2c1f0391ac3c6f343a2c28c/276650b7-nutty-shopper-t-501069.jpg?w=300&h=300&hfit=fill
+    /*
+    const asset = client.getAsset('50p1o2nZkNiQZYZGs1308H')
+    .then((asset) => console.log(`${asset.fields.file.url}?w=300&h=300&fit=fill`))  ;  
+    
+    const asset2 = client.getAsset('50p1o2nZkNiQZYZGs1308H')
+    .then((asset) => console.log(`${asset.fields.file.url}?w=300&h=200&fit=crop`))  ;  
+*/
+
+  return animals;
 }
 
 export default useAnimals;
+/*
+id: "50p1o2nZkNiQZYZGs1308H"
+https://www.contentful.com/developers/docs/references/images-api/#/reference/resizing-&-cropping/change-the-resizing-behavior/retrieve-an-image/console/js
+
+
+const asset = client.getAsset('<asset_id>')
+  .then((asset) => console.log(`${asset.fields.file.url}?fm=jpg&fl=progressive`))
+
+  const asset = client.getAsset('50p1o2nZkNiQZYZGs1308H')
+  .then((asset) => console.log(`${asset.fields.file.url}?w=100&h=100`))
+// function useMonsters() {
+//   const [monsters, setMonsters] = useState([]);
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [selectedMonster, setSelectedMonster] = useState({});
+
+//   useEffect(() => {
+//     fetch("https://www.dnd5eapi.co/api/monsters/")
+//       .then((response) => response.json())
+//       .then((json) => {
+//         setIsLoading(false);
+//         setMonsters(json.results);
+//       })
+//       .catch(() => console.log("Request failed"));
+//   }, []);
+
+//   const selectMonster = (monstersInfo, toggle) => {
+//     fetch(`https://www.dnd5eapi.co${monstersInfo.url}`)
+//       .then((response) => response.json())
+//       .then((json) => {
+//         setSelectedMonster({
+//           ...monstersInfo,
+//           alignment: json.alignment,
+//           type: json.type
+//         });
+//         toggle();
+//       })
+//       .catch(() => console.log("Request failed"));
+//   };
+
+//   return [monsters, selectedMonster, selectMonster, isLoading];
+// }
+
+// export default useMonsters;
+*/
