cf57189de06a62b7fcc5fa7b4b35c916eede76ba
Roberto Ubieto Juan <robertouj@gmail.com>
2021-04-12 18:10:33 +0200

    final changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./App.css";
+import "./custom.scss";
 import useAnimals from "./useAnimals";
 import React, { useState } from "react";
+import {
+  Container,  
+  Card,
+  CardImg,
+  CardBody,
+  CardTitle,
+  CardSubtitle
+} from "reactstrap";
 import {
   Button,
   ButtonGroup,
@@ -13,6 +22,7 @@ import AnimalsCarousel from "./components/AnimalsCarousel";
 import "bootstrap/dist/css/bootstrap.css";
 import YoutubeEmbed from "./components/YoutubeEmbed";
 import useSelectedAnimal from "./useSelectedAnimal";
+import logo from './img/Absolute_amazing_animals.png'
 
 function App() {
   // hook created to collect all the animals from the API
@@ -27,7 +37,7 @@ function App() {
   };
 
   const getSelectedAnimal = (e) => {
-    const {id, contentType} = e.target.dataset; //convention in React? To clarify with Patrick
+    const { id, contentType } = e.target.dataset; //convention in React? To clarify with Patrick
     fetchAnimal(id);
     setOptionToShow(contentType);
     toggle();
@@ -36,53 +46,77 @@ function App() {
   const modalContent = {
     carousel: <AnimalsCarousel animal={selectedAnimal} />,
     video: <YoutubeEmbed embedId={selectedAnimal.idVideo} />,
-  }
+  };
 
   return (
     <div className="App">
-      <div className="container">
-        <header>
-          <div className="wrapper"></div>
-          <span>Astonishing Animals</span>
-        </header>
-        <main className="Animal">
-          <div className="wrapper">
-            <div className="AnimalList">
-              {animals.map((animal, index) => {
-                return (
-                  <div key={index} className="Animal">
-                    <h3>{animal.name}</h3>
-                    <h2>{animal.latinName}</h2>
-                    <img src={animal.img} alt={animal.title} />
-                    <ButtonGroup>
-                      <Button data-id={animal.id} data-content-type="carousel" onClick={getSelectedAnimal}>
+      <header fluid>
+        <img src={logo} alt="Absolut amazing animals"/>
+        <span><h1>Absolute Amazing Animals</h1></span>
+      </header>
+      <main>
+        <Container>
+        <div className="wrapper">
+          <div className="AnimalList">
+            {animals.map((animal, index) => {
+              return (
+                <Card key={index} className="Animal shadow p-3 mb-5 bg-white rounded">
+                  <CardBody>
+                    <div className="card-content-top">
+                      <CardTitle tag="h3">{animal.name}</CardTitle>
+                      <CardImg
+                        bottom
+                        width="100%"
+                        src={animal.img}
+                        alt={animal.title}
+                      />
+                      {/* style={{fontSize: "24px"}} */}
+                      <CardSubtitle tag="h6" className="mb-2 pt-3 text-danger ">
+                        <i>{animal.latinName}</i>
+                      </CardSubtitle>
+                    </div>
+                    <ButtonGroup >
+                      <Button
+                        data-id={animal.id}
+                        data-content-type="carousel"
+                        onClick={getSelectedAnimal}
+                      >
                         Images
                       </Button>
-                      <Button data-id={animal.id} data-content-type="video" onClick={getSelectedAnimal}>Video</Button>
+                      <Button
+                        data-id={animal.id}
+                        data-content-type="video"
+                        onClick={getSelectedAnimal}
+                      >
+                        Video
+                      </Button>
                     </ButtonGroup>
-                  </div>
-                );
-              })}
-              <div className="Modal">
-                <Modal isOpen={modal} toggle={toggle}>
-                  <ModalHeader
-                    className="modal__header"
-                    toggle={toggle}
-                  ></ModalHeader>
-                  <ModalBody className="modal__body row">
-                   {!isLoading && modalContent[optionToShow]}
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button color="secondary" onClick={toggle}>
-                      Cancel
-                    </Button>
-                  </ModalFooter>
-                </Modal>
-              </div>
+                  </CardBody>
+
+                  {/* <img src={animal.img} alt={animal.title} /> */}
+                </Card>
+              );
+            })}
+            <div className="Modal">
+              <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader
+                  className="modal__header"
+                  toggle={toggle}
+                ></ModalHeader>
+                <ModalBody className="modal__body row">
+                  {!isLoading && modalContent[optionToShow]}
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="secondary" onClick={toggle}>
+                    Cancel
+                  </Button>
+                </ModalFooter>
+              </Modal>
             </div>
           </div>
-        </main>
-      </div>
+        </div>
+        </Container>
+      </main>
     </div>
   );
 }
