2c495c6357fd01e0856a39a3aeb201fc8c986dea
Roberto Ubieto Juan <robertouj@gmail.com>
2021-04-12 23:06:14 +0200

    integrated scss from reactstrap; modified primary bootstrap colors for personaliced palette; inserted new styles with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import "./App.css";
-import "./custom.scss";
 import useAnimals from "./useAnimals";
 import React, { useState } from "react";
 import {
-  Container,  
+  Container,
   Card,
   CardImg,
   CardBody,
   CardTitle,
-  CardSubtitle
+  CardSubtitle,
 } from "reactstrap";
 import {
   Button,
@@ -19,10 +17,11 @@ import {
   ModalFooter,
 } from "reactstrap";
 import AnimalsCarousel from "./components/AnimalsCarousel";
-import "bootstrap/dist/css/bootstrap.css";
 import YoutubeEmbed from "./components/YoutubeEmbed";
 import useSelectedAnimal from "./useSelectedAnimal";
-import logo from './img/Absolute_amazing_animals.png'
+import logo from "./img/Absolute_amazing_animals.png";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
 
 function App() {
   // hook created to collect all the animals from the API
@@ -44,77 +43,91 @@ function App() {
   };
 
   const modalContent = {
-    carousel: <AnimalsCarousel animal={selectedAnimal} />,
+    carousel: <AnimalsCarousel animal={selectedAnimal}/>,
     video: <YoutubeEmbed embedId={selectedAnimal.idVideo} />,
   };
 
   return (
-    <div className="App">
+    <div className="App bg-secondary">
       <header fluid>
-        <img src={logo} alt="Absolut amazing animals"/>
-        <span><h1>Absolute Amazing Animals</h1></span>
+        <img src={logo} alt="Absolut amazing animals" />
+        <span>
+          <h1>Absolute Amazing Animals</h1>
+        </span>
       </header>
       <main>
         <Container>
-        <div className="wrapper">
-          <div className="AnimalList">
-            {animals.map((animal, index) => {
-              return (
-                <Card key={index} className="Animal shadow p-3 mb-5 bg-white rounded">
-                  <CardBody>
-                    <div className="card-content-top">
-                      <CardTitle tag="h3">{animal.name}</CardTitle>
-                      <CardImg
-                        bottom
-                        width="100%"
-                        src={animal.img}
-                        alt={animal.title}
-                      />
-                      {/* style={{fontSize: "24px"}} */}
-                      <CardSubtitle tag="h6" className="mb-2 pt-3 text-danger ">
-                        <i>{animal.latinName}</i>
-                      </CardSubtitle>
-                    </div>
-                    <ButtonGroup >
-                      <Button
-                        data-id={animal.id}
-                        data-content-type="carousel"
-                        onClick={getSelectedAnimal}
-                      >
-                        Images
-                      </Button>
-                      <Button
-                        data-id={animal.id}
-                        data-content-type="video"
-                        onClick={getSelectedAnimal}
-                      >
-                        Video
-                      </Button>
-                    </ButtonGroup>
-                  </CardBody>
+          <div className="wrapper">
+            <div className="AnimalList">
+              {animals.map((animal, index) => {
+                return (
+                  <Card
+                    key={index}
+                    className="Animal shadow p-3 mb-5 bg-primary rounded-lg"
+                  >
+                    <CardBody>
+                      <div className="card-content-top">
+                        <CardTitle tag="h3" className="text-third">{animal.name}</CardTitle>
+                        <CardImg
+                          bottom
+                          width="100%"
+                          src={animal.img}
+                          alt={animal.title}
+                        />
+                        {/* style={{fontSize: "24px"}} */}
+                        <CardSubtitle
+                          tag="h6"
+                          className="mb-2 pt-3"
+                        >
+                          <i className=" text-third">{animal.latinName}</i>
+                        </CardSubtitle>
+                      </div>
+                      <ButtonGroup>
+                        <Button
+                          className="bg-third text-fourth"
+                          data-id={animal.id}
+                          data-content-type="carousel"
+                          onClick={getSelectedAnimal}
+                        >
+                          Images
+                        </Button>
+                        <Button
+                          className="bg-third text-fourth"
+                          data-id={animal.id}
+                          data-content-type="video"
+                          onClick={getSelectedAnimal}
+                        >
+                          Video
+                        </Button>
+                      </ButtonGroup>
+                    </CardBody>
 
-                  {/* <img src={animal.img} alt={animal.title} /> */}
-                </Card>
-              );
-            })}
-            <div className="Modal">
-              <Modal isOpen={modal} toggle={toggle}>
-                <ModalHeader
-                  className="modal__header"
-                  toggle={toggle}
-                ></ModalHeader>
-                <ModalBody className="modal__body row">
-                  {!isLoading && modalContent[optionToShow]}
-                </ModalBody>
-                <ModalFooter>
-                  <Button color="secondary" onClick={toggle}>
-                    Cancel
-                  </Button>
-                </ModalFooter>
-              </Modal>
+                    {/* <img src={animal.img} alt={animal.title} /> */}
+                  </Card>
+                );
+              })}
+              <div className="Modal">
+                <Modal isOpen={modal} toggle={toggle} className="bg-primary rounded-lg modal-lg">
+                  <ModalHeader
+                    className="bg-primary bg-third rounded-top"
+                    toggle={toggle}
+                  ></ModalHeader>
+                  <ModalBody className="bg-primary justify-content-center px-2" >
+                    {!isLoading && modalContent[optionToShow]}
+                  </ModalBody>
+                  <ModalFooter className="bg-primary">
+                    <Button
+                      color="secondary"
+                      onClick={toggle}
+                      className="bg-third text-fourth"
+                    >
+                      Cancel
+                    </Button>
+                  </ModalFooter>
+                </Modal>
+              </div>
             </div>
           </div>
-        </div>
         </Container>
       </main>
     </div>
